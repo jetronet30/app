568a4190566c83ac0a3741e1997b2c6a1b6e25b4
jetronet30 <jabakhuluzauri@gmail.com>
2026-05-25 16:37:52 +0400

    dja_1

--- app/urls.py
+++ app/urls.py
@@ -1,11 +1,9 @@
-
 from django.contrib import admin
 from django.http import request
-from django.urls import path
+from django.urls import include, path
 from main import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
+    path("admin/", admin.site.urls),
+    path("", include("main.urls", namespace="main")),
 ]
--- main/views.py
+++ main/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 
 def index(request):
     context = {
-        'title': 'Home',
-        'content': 'Welcome to the Home page',
-        'list':['item1', 'item2', 'item3'],
-        'dict': {'key1': 'value1', 'key2': 'value2'},
-        'is_active': False
+        'title': 'Home - главная ',
+        'content': 'Магазин мебели HOME'
+
     }
     return render(request, 'main/index.html', context)
 
 def about(request):
-    return render(request, 'main/about.html')
+    context = {
+        'title': 'Home - о нас',
+        'content': 'O нас',
+        'text_on_page': ' text about us and our company'
+
+    }
+    return render(request, 'main/about.html', context)
