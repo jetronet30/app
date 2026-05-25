660db83561253bc3c1559dd3d9303700a6e53e94
jetronet30 <jabakhuluzauri@gmail.com>
2026-05-25 17:43:59 +0400

    dja_2

--- app/urls.py
+++ app/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.http import request
 from django.urls import include, path
 from main import views
+from goods import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls", namespace="main")),
+    path("catalog/", include("goods.urls", namespace="catalog")),
 ]
