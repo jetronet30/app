d818e67df1672ab0f0b84a9e555a8efc88370d38
jetronet30 <jabakhuluzauri@gmail.com>
2026-05-29 17:08:02 +0400

    dja_7

--- goods/urls.py
+++ goods/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
 
-app_name = 'catalog'
+app_name = 'goods'
 
 urlpatterns = [
     path('', views.catalog, name='index'),
     path('product/', views.product, name='product'),
+    path('product/<slug:product_slug>/', views.product, name='product'),
 ]
--- goods/views.py
+++ goods/views.py
@@ -12,5 +12,9 @@ def catalog(request):
 
 
  
-def product(request):
-    return render(request, 'goods/product.html')
+def product(request, product_slug):
+    product = Products.objects.get(slug=product_slug)
+    context = {
+        'product': product,
+    }
+    return render(request, 'goods/product.html', context=context)
